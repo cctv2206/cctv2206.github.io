ea19f1afe3d6d965307ab887d717b7a8fef47aa5
Kai Kang <cctv2206@outlook.com>
2018-04-08 17:35:01 -0700

    Index page added.

---
+++ _next/fcb484e1-d81e-43b2-907c-1c5e08c4f770/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([6],{869:function(e,t,u){e.exports=u(870)},870:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(20),a=n(l),r=u(4),d=n(r),f=u(5),o=n(f),c=u(6),i=n(c),s=u(7),p=n(s),_=u(0),m=n(_),h=u(195),v=(n(h),u(50),u(90)),x=n(v),y=(u(63),function(e){function t(){return(0,d.default)(this,t),(0,i.default)(this,(t.__proto__||(0,a.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,o.default)(t,[{key:"render",value:function(){return m.default.createElement(x.default,null,m.default.createElement("div",null,m.default.createElement("h3",null,"Index Page")))}}]),t}(_.Component));t.default=y}},[869]);
+            return { page: comp.default }
+          })
+        
